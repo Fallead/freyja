bc7e5c42a8ab12d185a9fb74c4d2c1cd283dfac1
wangdi <wangdi@inad.com>
2017-10-22 21:44:00 +0800

    add function:login

--- freyja/views.py
+++ freyja/views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import logging
 
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 from utils.permissionmixin import PermissionMixin
 
@@ -10,15 +13,37 @@ logger = logging.getLogger(__name__)
 
 
 def login(request):
-    pass
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('index'))
+    if request.method == 'POST':
+        user = authenticate(
+            username=request.POST['username'],
+            password=request.POST['password']
+        )
+        if user and user.is_active:
+            auth_login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, template_name, {'errors': True})
+    else:
+        return render(request, 'login.html')
 
 
 def logout(request):
-    pass
+    auth_logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
 
 def signup(request):
-    pass
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('index'))
+    if request.method == 'POST':
+        print request.POST.get('username')
+        print request.POST.get('email')
+        print request.POST.get('password')
+        return HttpResponse('ok')
+    else:
+        return render(request, 'signup.html')
 
 
 def forget(request):
